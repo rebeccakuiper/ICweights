3b09c919bde2bb3967f91a002072b6e528183f44
rebeccakuiper <46786848+rebeccakuiper@users.noreply.github.com>
2021-04-07 14:45:12 +0200

    caption in print output

--- R/print_ICw.R
+++ R/print_ICw.R
@@ -16,6 +16,10 @@ print.ICw <- function(x, digits = NULL)
   DF <- data.frame(IC = x$IC,
                    IC.weights = x$IC.weights,
                    rel.IC.weights = x$rel.IC.weights)
+
+  cat("\n")
+  cat("Per hypothesis/model, the information criterion value (IC), its weight (IC.weights), and its relative weight (rel.IC.weights) versus the other hypotheses/models: \n")
+  cat("\n")
   print(DF, digits = NrDigits, right = F)
 
   return(invisible(DF))
