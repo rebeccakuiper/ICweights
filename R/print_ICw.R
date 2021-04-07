a44743e763c88053d0deffe3d7e2a5b31dbf3681
rebeccakuiper <46786848+rebeccakuiper@users.noreply.github.com>
2021-04-07 14:34:12 +0200

    Number of digits in output

--- R/print_ICw.R
+++ R/print_ICw.R
@@ -2,16 +2,24 @@
 #' @export print.ICw
 #' @export
 
-print.ICw <- function(x)
+print.ICw <- function(x, digits = NULL)
 {
 
   x <- as.list(x)
+
+  if(is.null(digits)){
+    NrDigits <- 3
+  }else{
+    NrDigits <- digits
+  }
+
   DF <- data.frame(IC = x$IC,
                    IC.weights = x$IC.weights,
                    rel.IC.weights = x$rel.IC.weights)
-  print(DF, digits = 4, right = F)
+  print(DF, digits = NrDigits, right = F)
 
   return(invisible(DF))
+  #return(invisible(x))
 
 }
 
--- R/summary_ICw.R
+++ R/summary_ICw.R
@@ -3,13 +3,22 @@
 #' @export summary.ICw
 #' @export
 
-summary.ICw <- function(x)
+summary.ICw <- function(x, digits = NULL)
 {
   x <- as.list(x)
+
+  if(is.null(digits)){
+    NrDigits <- 3
+    sig.digits <- TRUE
+  }else{
+    NrDigits <- digits
+    sig.digits <- FALSE
+  }
+
   DF <- data.frame(IC = x$IC,
                    IC.weights = x$IC.weights,
                    rel.IC.weights = x$rel.IC.weights)
 
-  md_table(DF, digits = 2, align = 'c') # TO DO set nr digit as input value
+  md_table(DF, digits = NrDigits, sig.digits = sig.digits, align = 'c')
 
 }
