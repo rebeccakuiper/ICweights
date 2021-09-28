7b0f2c445973cd382f2bfb0caa633fbd49a891c9
rebeccakuiper <46786848+rebeccakuiper@users.noreply.github.com>
2021-09-28 12:24:48 +0200

    Added PTweights example

--- R/ICweights.r
+++ R/ICweights.r
@@ -17,6 +17,7 @@
 #' IC <- myIC # Example based on 3 hypotheses.
 #' IC.weights(IC)
 #'
+#'
 #' # Change labels of hypotheses #
 #' #
 #' # Example 1: Let us say that we evaluated H1: beta1 > beta2; H2: beta1 = beta2; and H3: beta1 < beta2.
@@ -31,6 +32,9 @@
 #' Name_Hypo <- c("Linear", "Quadratic", "Cubic")
 #' IC.weights(IC, Name_Hypo)
 #'
+#'
+#' # Output options #
+#' #
 #' # Example 3: This examples shows some output options
 #' ICw <- IC.weights(IC)
 #' print(ICw)
@@ -42,6 +46,27 @@
 #' ICw$IC.weights
 #' ICw$ratio.IC.weights
 #'
+#'
+#' # PT weights #
+#' #
+#' # Example 4: This examples shows how to calculate PT weights.
+#' # Notably, one is interested in PT weights when the log likelihood for two or more hypotheses are (approximately) equal.
+#' # Then, the comparison between those hypotheses is solely based on the PT values.
+#' # The IC weights will then equal the PT weights.
+#' # In that case, there is support for the overlap (boundary) of these hypotheses.
+#' # Thus, when the IC weights equal the PT weights for a (sub)set of hypotheses,
+#' # then there is support for the overlap (boundary) of these hypotheses.
+#' #
+#' # Example code:
+#' # est <- coef(fit.object)
+#' # VCOV_est <- vcov(fit.object)
+#' # results <- goric(est, VCOV = VCOV_est, H1, comparison = "complement", type = "gorica")
+#' # IC.weights(2*results$result[,3])$IC.weights
+#' ## Note that the penalty is 2*`penalty'
+#' ## So, do not use: IC.weights(results$result[,3])$IC.weights
+#'
+
+
 
 
 IC.weights <- function(IC, Name_Hypo = NULL) {
